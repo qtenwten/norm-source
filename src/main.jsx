f18769ae450beefa77d266d7e0dcfd1d104a19bf
qtenwten <66746672+qtenwten@users.noreply.github.com>
2026-09-14 15:50:20 +0300

    Load v4 interaction styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './styles.css'
 import './v2.css'
 import './cinematic.css'
 import './v3.css'
+import './v4.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
