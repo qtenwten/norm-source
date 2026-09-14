feefdc944463a94130d25ceb8808f16e665af9ea
qtenwten <66746672+qtenwten@users.noreply.github.com>
2026-09-15 02:31:17 +0300

    Load archive clearance portal styles

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import './v7.css'
 import './v8.css'
 import './v9.css'
 import './v10.css'
+import './v11.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
