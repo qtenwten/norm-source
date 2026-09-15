8f47a37184cd385841775e25a16d0dd8acd8c85c
qtenwten <66746672+qtenwten@users.noreply.github.com>
2026-09-15 14:35:48 +0300

    Load humorous field-team message thread

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import './funMessagesPatch.js'
 import App from './App.jsx'
 import './styles.css'
 import './v2.css'
